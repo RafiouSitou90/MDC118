cdcde645f04f608b262f0ca3c314e32e7d122cc0
Rafiou Sitou <rafiousitou90@gmail.com>
2020-09-09 21:42:11 -0300

    End of the declaracao.c file.

--- Aulas/02-VariaveisConstantes/declaracao.c
+++ Aulas/02-VariaveisConstantes/declaracao.c
@@ -3,15 +3,25 @@
 
 // Declaration of global variables.
 
+char ch = 'A';                              // -128 to 127
+unsigned char var = 10;                     // 0 to 255
+
 int number = 100;
-char ch = 'A'; // -128 to 127
-unsigned char var = 10; // 0 to 255
-const int GENDER_MALE = 1; // Constant
+short int shortNumber = 100;                // -32768 to 32767 (short int number)
+long int longNumber = 1000000000L;          // long int number
+
+float decimalNumber = 0.123456;             // float number (06 decimal after the coma)
+double doubleNumber = 0.123451234512345;    // double number (15 decimals after the coma)
+
+const int GENDER_MALE = 1;                  // Constant
 
 int main (int argc, const char * argv[]) {
 
-    printf("Number: %d\n", number);
     printf("Ch: %c\n", ch);
+    printf("Ch: %d\n", var);
+    printf("Number: %d\n", number);
+    printf("Float Number: %.6f\n", decimalNumber);
+    printf("Double Number: %.15f\n", doubleNumber);
 
     system("PAUSE");
     return 0;
