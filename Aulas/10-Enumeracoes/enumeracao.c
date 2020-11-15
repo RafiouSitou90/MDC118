64595e32a903f12ad1bb6e948837fd8dbfe4a012
Rafiou Sitou <rafiousitou90@gmail.com>
2020-11-14 23:42:01 -0300

    feat(aulas): Add 10-Enumeracoes.
    - Add enumeracao.c

---
+++ Aulas/10-Enumeracoes/enumeracao.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+enum dias { dom, seg, ter, qua, qui, sex, sab };
+enum genero { masculino = 1, feminino = 2 };
+enum status {ativo = 1, inativo = 0};
+
+int main(int argc, const char *argv[]) {
+
+    printf("\n\n");
+    system("PAUSE");
+
+    return 0;
+}
