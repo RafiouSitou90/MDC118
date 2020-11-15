f09baa612d9de60877cd21b1bbcbdb03fafbbad5
Rafiou Sitou <rafiousitou90@gmail.com>
2020-11-15 00:38:21 -0300

    feat(atividades): Update Atividade05 resposta_01 and resposta_03.
    - Update resposta_01.c
    - Update resposta_03.c

--- Atividades/Atividade05/resposta_03.c
+++ Atividades/Atividade05/resposta_03.c
@@ -8,22 +8,26 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 const int SIZE = 80;
 
 int main(int argc, const char *argv[]) {
 
-    char character[SIZE];
+    char characters[SIZE];
 
-    for (int i = 0; i < SIZE; i++) {
-        printf("Enter the character %d :", i + 1);
-        scanf("%c", &character[i]);
-        getchar();
+    printf("Enter the characters (ex: max characters = %d) :", SIZE);
+    gets(characters);
+
+    while (strlen(characters) <= 0 || SIZE < strlen(characters)) {
+        printf("Max characters allowed :%d, try again\n", SIZE);
+        printf("Enter the characters (ex: max characters = %d):", SIZE);
+        gets(characters);
     }
-
     printf("\n");
-    for (int i = SIZE; i > 0; i--) {
-        printf("Character %d : %c\n", i, character[i - 1]);
+
+    for (int i = strlen(characters) - 1; i >= 0; i--) {
+        printf("%c", characters[i]);
     }
 
     printf("\n\n");
